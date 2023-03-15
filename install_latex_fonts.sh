7bda8d79701213f50cdab8e59a3e77e73e4be35b
Abdur R. Fayjie <fayjie92@yahoo.com>
2023-03-15 17:07:54 +0100

    latex fonts

---
+++ install_latex_fonts.sh
@@ -0,0 +1,8 @@
+# Install CM font
+wget --no-parent -r --reject "index.html*" "https://mirror.lyrahosting.com/CTAN/fonts/cm/ps-type1/bakoma/otf/"
+
+# Install LM font
+wget --no-parent -r --reject "index.html*" "https://ftp.snt.utwente.nl/pub/software/tex/fonts/lm/fonts/opentype/public/lm"
+
+
+#mv /home/far/ftp.snt.utwente.nl/pub/software/tex/fonts/lm/fonts/opentype/public/lm
